7d4bd5a19819a391bf3a9d979bb5da78973b9700
thanhdt-viosoft <thanhdt@viosoft.com>
2017-02-16 18:13:28 +0700

    Fix for file uploading

--- index.js
+++ index.js
@@ -8,11 +8,13 @@ const app = express();
 app.use(cors());
 // app.options('*', cors())
 
+Error.BAD_REQUEST = 400;
 Error.NOT_FOUND = 404;
 Error.EXPIRED = 440;
 Error.AUTHEN = 401;
 Error.AUTHORIZ = 403;
 Error.INTERNAL = 500;
+Error.CONDITION = 412;
 Error.create = (code, msg) => {
     let err = new Error(msg);
     err.status = code;
