aa9f7a9a546f109034bc640dca369d3be05f5902
thanhdt-viosoft <thanhdt@viosoft.com>
2017-02-21 15:03:26 +0700

    FIx http request via restify error

--- env/development.js
+++ env/development.js
@@ -2,6 +2,6 @@ const db = require('../src/db');
 
 console.log('This is development mode');
 app.all('/*', (req, res, next) => {
-    console.log(db.info());
+    console.log('-', `Mongo: ${db.size()}`);
     next();
 });
