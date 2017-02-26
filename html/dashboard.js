b2b5f3043db95eaa504172d87c985bf43727a711
Doan Thuan Thanh <Doan Thuan Thanh>
2017-02-26 14:44:16 +0700

    Show project meny when role is super admin

--- html/dashboard.js
+++ html/dashboard.js
@@ -9,17 +9,19 @@ window.handlerError = (res) => {
     if(res.error) {
         alert(res.error);
         location.href = `/?id=${window.localStorage.pj}`;
-    }else if(res.currentProject) {
+    }else if(res.currentProject) {        
         window.sessionStorage.currentProject = JSON.stringify(res.currentProject);
-        document.querySelector('#projectInfo').innerText = res.currentProject.name;
         let actionButtons = [];
         for(let k in window.config.plugins){
             if(!window.config.plugins[k].isMenu) continue;     
-            if(!(window.sessionStorage.currentProject instanceof Array) && k === 'project') continue;
-            else window.sessionStorage.currentProject[0];
+            if(k === 'project') {
+                if(!(res.currentProject instanceof Array)) continue;
+                else res.currentProject = res.currentProject[0];
+            }
             actionButtons.push(`<li><a href="${window.config.plugins[k].link}?session=${token}&theme=${theme}" target="mainFrame">${window.config.plugins[k].name}</a></li>`);
         }        
         actions.innerHTML = actionButtons.join('');
+        document.querySelector('#projectInfo').innerText = res.currentProject.name;
     }else if(res.logout){
         location.href = `/?id=${window.localStorage.pj}`;
     }
