eacb37b0634f20314bf375bd5d68a36331fd2033
thanhdt-viosoft <thanhdt@viosoft.com>
2017-02-28 16:28:07 +0700

    Fix config debug for db and cached in queue

--- env/development.js
+++ env/development.js
@@ -3,10 +3,5 @@ const cachedService = require('../src/service/cached.service');
 
 console.log('This is development mode');
 setInterval(() => {
-<<<<<<< HEAD
     console.log('-', `Mongo: ${db.size()}`, `Cached: ${cachedService.size()}`);
 }, 5000);
-=======
-    console.log('-', `Mongo: ${db.size()}`);
-}, 1000);
->>>>>>> e501e368d9e04e3e034fd8bd20dc008fdf95ac0f
