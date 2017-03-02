3ee563c47ca2af4d2abee3d2ad281e3656c3c77b
thanhdt-viosoft <thanhdt@viosoft.com>
2017-03-02 15:50:59 +0700

    Remove unused file in master

--- src/appconfig.js
+++ src/appconfig.js
@@ -1,11 +1,7 @@
 global.appconfig = {
-    name: 'plugin.core',
     listen: 9002,
     db: {
         url: 'mongodb://localhost:27017/core'
-    },
-    auth: {
-        url: 'http://localhost:9600'
     },
     app: {
         
--- src/service/http.service.js
+++
@@ -1,5 +0,0 @@
-const _ = require('lodash');
-
-exports = module.exports = _.extend(require('../../lib/core/http.service'), {
-
-});
